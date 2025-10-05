4bd662a5394c9bc81e89e32a729d0b4cf9a1fb4d
deepshikha <deepshikha@antiersolutions.com>
2025-10-05 11:54:40 +0530

    change logo

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { lazy, Suspense } from "react";
+import { lazy } from "react";
 import Layout from "../Components/Layout/Layout";
 
 // Lazy-loaded components
